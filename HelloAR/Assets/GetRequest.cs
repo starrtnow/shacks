9ac4cfad5f300b111b8a8eb9dfc7e6d5f285bb3d
Starr Yang <starrtyang@berkeley.edu>
2018-04-15 10:00:43 -0700

    wooops wron gclient

---
+++ HelloAR/Assets/GetRequest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class GetRequest : MonoBehaviour {
+
+    public TextMesh text;
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(Upload());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator Upload()
+    {
+        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+        formData.Add(new MultipartFormDataSection("image", "test"));
+
+        UnityWebRequest www = UnityWebRequest.Post("138.68.230.6:5000/nutri/", formData);
+        yield return www.SendWebRequest();
+
+        if(www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log(www.downloadHandler.text);
+        }
+    }
+}
---
+++ HelloAR/Assets/GoogleARCore/Examples/HelloAR/Scripts/PointcloudVisualizer.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="PointcloudVisualizer.cs" company="Google">
+//
+// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace GoogleARCore.HelloAR
+{
+    using System.Collections.Generic;
+    using GoogleARCore;
+    using UnityEngine;
+    using UnityEngine.Profiling;
+
+    /// <summary>
+    /// Visualize the point cloud.
+    /// </summary>
+    public class PointcloudVisualizer : MonoBehaviour
+    {
+        private const int k_MaxPointCount = 61440;
+
+        private Mesh m_Mesh;
+
+        private Vector3[] m_Points = new Vector3[k_MaxPointCount];
+
+        /// <summary>
+        /// Unity start.
+        /// </summary>
+        public void Start()
+        {
+            m_Mesh = GetComponent<MeshFilter>().mesh;
+            m_Mesh.Clear();
+        }
+
+        /// <summary>
+        /// Unity update.
+        /// </summary>
+        public void Update()
+        {
+
+            // Fill in the data to draw the point cloud.
+
+        }
+    }
+}
